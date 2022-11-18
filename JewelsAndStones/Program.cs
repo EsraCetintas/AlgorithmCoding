3785583057df72db213ce60441e2200ce6ea7e22
Esra Çetintaş <Esra.Cetintas@outlook.com.tr>
2022-11-18 19:49:59 +0300

    Add project files.

---
+++ JewelsAndStones/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace JewelsAndStones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jewels = Console.ReadLine();
+            string stones = Console.ReadLine();
+            int sayac = 0;
+
+            for (int i = 0; i < stones.Length; i++)
+            {
+                for (int j = 0; j < jewels.Length; j++)
+                {
+                    if (stones[i] == jewels[j])
+                    {
+                        sayac++;
+                    }
+                }
+            }
+
+            Console.WriteLine(sayac);
+        }
+    }
+}
---
+++ RansomNote/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RansomNote
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ransomNote = Console.ReadLine();
+            string magazine = Console.ReadLine();
+
+            //Dictionary<Key_Tipi, Value_Tipi> Referans_Adi = new Dictionary<Key_Tipi, Value_Tipi>();
+            var dict = new Dictionary<char, int>();
+
+            foreach (var c in magazine)
+            {
+                if (dict.ContainsKey(c))
+                {
+                    dict[c]++;
+                }
+                else
+                {
+                    dict.Add(c, 1);
+                }
+            }
+
+            foreach (var c in ransomNote)
+            {
+                if (!dict.ContainsKey(c) || dict[c] <= 0 )
+                {
+                    Console.WriteLine("false");
+                }
+                dict[c]--;
+            }
+
+            Console.WriteLine(true);
+
+        }
+    }
+}
