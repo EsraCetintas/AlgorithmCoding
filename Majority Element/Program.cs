f8e13c3b8c3922804a9a3c00705c2634842050b8
Esra Çetintaş <Esra.Cetintas@outlook.com.tr>
2022-11-22 00:06:25 +0300

    majority-element problem was done

---
+++ Majority Element/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MajorityElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            List<int> nums = new List<int>();
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                nums.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            foreach (var num in nums)
+            {
+
+                if (dict.ContainsKey(num))
+                {
+                    dict[num]++;
+                }
+                else
+                {
+                    dict.Add(num, 1);
+                }
+            }
+
+            foreach (var d in dict.Keys)
+            {
+                if (dict[d] > n / 2)
+                {
+                    Console.WriteLine(d);
+                }
+            }
+
+        }
+    }
+}
